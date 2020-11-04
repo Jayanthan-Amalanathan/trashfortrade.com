beee63c12ade75fc4a2672fb3884dd5a2443d22f
Abitharani.J <abitharani009@gmail.com>
2020-11-04 11:51:02 +0530

    Delete php form

--- pages/1-f-contact.php
+++ pages/1-f-contact.php
@@ -61,11 +61,6 @@
               <textarea class="form-control" name="message" rows="5" name="entry.1232062195" required></textarea>
               <div class="validate"></div>
             </div>
-            <div class="mb-3">
-              <div class="loading">Loading</div>
-              <div class="error-message"></div>
-              <div class="sent-message">Your message has been sent. Thank you!</div>
-            </div>
             <div class="text-center"><button type="submit">Send Message</button></div>
           </form>
         </div>
