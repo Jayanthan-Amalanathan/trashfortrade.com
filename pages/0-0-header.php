d21968cf1a2b82206614e1b38f828ae325ee2cc8
Jayanthan Amalanathan <ajayanthan97@gmail.com>
2020-11-05 07:27:11 +0530

    Add new web files

---
+++ pages/0-0-header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="utf-8">
+ <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+ <title>Trash For Trade</title>
+ <meta content="" name="descriptison">
+ <meta content="" name="keywords">
+
+ <!-- Favicons -->
+ <link href="../media/icons/logo-icon.png" rel="logo-icon">
+ <link href="../media/icons/logo-trash-for-trade.png" rel="logo">
+
+ <!-- Google Fonts -->
+ <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+
+ <!-- Vendor CSS Files -->
+ <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+ <link href="../assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+ <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+ <link href="../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+ <link href="../assets/vendor/venobox/venobox.css" rel="stylesheet">
+ <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+ <script type="text/javascript" src="../assets/js/main.js"></script>
+ <script type="text/javascript" src="../assets/js/google-form.js"></script>
+
+ <!-- Main CSS File -->
+ <link href="../assets/css/style.css" rel="stylesheet">
+ <link href="../assets/css/about.css" rel="stylesheet">
+ <link href="../assets/css/services.css" rel="stylesheet">
+ <link href="../assets/css/features.css" rel="stylesheet">
+ <link href="../assets/css/contact.css" rel="stylesheet">
+ <link href="../assets/css/solution.css" rel="stylesheet">
+ <link href="../assets/css/testimonials.css" rel="stylesheet">
+ <link href="../assets/css/team.css" rel="stylesheet">
+ <?php include('0-1-cookie-consent.php'); ?>
+
+</head>
+
+<body>
+<?php include('0-2-menu-bar.php'); ?>
---
+++ pages/0-2-menu-bar.php
@@ -0,0 +1,32 @@
+<!-- ======= Header ======= -->
+<div id="header" class="header-transparent">
+  <div class="container">
+
+    <div id="logo" class="pull-left">
+      <a href="1-0-index.php"><img src="../media\icons\icon-navigation-bar-trashfortrade.png" alt=""></a>
+
+      <!-- ======= Navigation Bar ======= -->
+    </div>
+
+    <nav id="nav-menu-container">
+      <ul class="nav-menu">
+        <li class="menu-active"><a href="1-0-index.php">Home</a></li>
+        <li class="menu-has-children"><a href="1-0-index.php#about">About Us</a>
+          <ul>
+            <li><a href="2-0-about.php#team" target="_self">Team</a></li>
+            <li><a href="2-0-about.php#supporters">Supporters</a></li>
+            <li><a href="2-0-about.php#partners">Partners</a></li>
+            <li><a href="2-0-about.php#achievement">Achievement</a></li>
+
+          </ul>
+        </li>
+        <li><a href="1-0-index.php#solution">Solution</a></li>
+        <li><a href="1-0-index.php#services">Services</a></li>
+        <li><a href="1-0-index.php#blog">Blog</a></li>
+        <li><a href="1-0-index.php#contact">Contact Us</a></li>
+      </ul>
+    </nav>
+    <!-- #nav-menu-container -->
+  </div>
+</div>
+<!-- End Header -->
