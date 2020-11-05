06e849597791d0516638f59bb2797eb3503053b9
Abitharani.J <abitharani009@gmail.com>
2020-11-05 08:19:12 +0530

    Add Services section

---
+++ pages/1-d-services.php
@@ -0,0 +1,125 @@
+<!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio section-bg">
+      <div class="container" data-aos="fade-up">
+        <div class="section-header">
+          <h3 class="section-title">Services</h3>
+        </div>
+
+    <div class="row portfolio-container">
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+        <div class="portfolio-wrap">
+          <img src="../media\images\title-images-trash-for-trade\title-images-trash-for-trade-5.jpg" class="img-fluid" alt="">
+          <div class="portfolio-info">
+            <h4>App 1</h4>
+            <p>App</p>
+            <div class="portfolio-links">
+              <a href="assets/img/portfolio/portfolio-1.jpg" data-gall="portfolioGallery" class="venobox" title="App 1"><i class="bx bx-plus"></i></a>
+              <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+        <div class="portfolio-wrap">
+          <img src="../media\images\title-images-trash-for-trade\title-images-trash-for-trade-5.jpg" class="img-fluid" alt="">
+          <div class="portfolio-info">
+            <h4>Web 3</h4>
+            <p>Web</p>
+            <div class="portfolio-links">
+              <a href="assets/img/portfolio/portfolio-2.jpg" data-gall="portfolioGallery" class="venobox" title="Web 3"><i class="bx bx-plus"></i></a>
+              <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+        <div class="portfolio-wrap">
+          <img src="../media\images\title-images-trash-for-trade\title-images-trash-for-trade-5.jpg" class="img-fluid" alt="">
+          <div class="portfolio-info">
+            <h4>App 2</h4>
+            <p>App</p>
+            <div class="portfolio-links">
+              <a href="assets/img/portfolio/portfolio-3.jpg" data-gall="portfolioGallery" class="venobox" title="App 2"><i class="bx bx-plus"></i></a>
+              <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</section><!-- End Portfolio Section -->
+
+<!-- ======= Testimonials Section ======= -->
+<section id="testimonials" class="testimonials section-bg">
+  <div class="container" data-aos="fade-up">
+    <div class="section-header">
+      <h3 class="section-title">Testimonials</h3>
+    </div>
+
+    <div class="owl-carousel testimonials-carousel">
+
+      <div class="testimonial-item">
+        <img src="../media\images\logo-trash-for-trade.png" class="testimonial-img" alt="">
+        <h3>Dave Hakkins</h3>
+        <h4>Ceo &amp; Founder</h4>
+        <p>
+          <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+          Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+          <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+        </p>
+      </div>
+
+      <div class="testimonial-item">
+        <img src="../media\images\logo-trash-for-trade.png" class="testimonial-img" alt="">
+        <h3>MEPA Director - Batticaloa</h3>
+        <h4>Designer</h4>
+        <p>
+          <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+          Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+          <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+        </p>
+      </div>
+
+      <div class="testimonial-item">
+        <img src="../media\images\logo-trash-for-trade.png" class="testimonial-img" alt="">
+        <h3>Heminda Jayaweera</h3>
+        <h4>Store Owner</h4>
+        <p>
+          <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+          Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+          <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+        </p>
+      </div>
+
+      <div class="testimonial-item">
+        <img src="../media\images\logo-trash-for-trade.png" class="testimonial-img" alt="">
+        <h3>UNDP Team</h3>
+        <h4>Freelancer</h4>
+        <p>
+          <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+          Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+          <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+        </p>
+      </div>
+
+      <div class="testimonial-item">
+        <img src="../media\images\logo-trash-for-trade.png" class="testimonial-img" alt="">
+        <h3>Squarehub Team</h3>
+        <h4>Entrepreneur</h4>
+        <p>
+          <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+          Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+          <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+        </p>
+      </div>
+
+    </div>
+
+  </div>
+</section>
+<!-- End Testimonials Section -->
