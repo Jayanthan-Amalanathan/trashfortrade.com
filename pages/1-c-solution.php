9e437f29e4316c3509142fa5cc355c195b08107d
Abitharani.J <abitharani009@gmail.com>
2020-11-12 01:20:00 +0530

    change features css

--- pages/1-c-solution.php
+++ pages/1-c-solution.php
@@ -9,44 +9,44 @@
       <div class="col-lg-4 col-md-6" data-aos="zoom-in">
         <div class="box">
           <div class="icon"><i class='bx bx-water'></i></div>
-          <h4 class="title"><a href="">Marine Conservation</a></h4>
+          <h4 class="title">Marine Conservation</h4>
           <p class="description">Reduce the plastic waste ended up in inland water bodies and ocean</p>
         </div>
       </div>
       <div class="col-lg-4 col-md-6" data-aos="zoom-in">
         <div class="box">
           <div class="icon"><i class='bx bx-recycle'></i></div>
-          <h4 class="title"><a href="">Eco Friendly</a></h4>
+          <h4 class="title">Eco Friendly</h4>
           <p class="description">Opportunity to eliminate plastic waste and reduce the demand for virgin plastic</p>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6" data-aos="zoom-in">
-        <div class="box">
-          <div class="icon"><i class='bx bx-group'></i></div>
-          <h4 class="title"><a href="">Community</a></h4>
-          <p class="description">They provide support and create awareness among locals for a better livelihood</p>
         </div>
       </div>
 
       <div class="col-lg-4 col-md-6" data-aos="zoom-in">
         <div class="box">
           <div class="icon"><i class='bx bxs-business'></i></i></div>
-          <h4 class="title"><a href="">Mix Workspace</a></h4>
+          <h4 class="title">Mix Workspace</h4>
           <p class="description">Combination collection, machine-building, and upcycling spaces infrastructure framework</p>
         </div>
       </div>
       <div class="col-lg-4 col-md-6" data-aos="zoom-in">
         <div class="box">
           <div class="icon"><i class='bx bxs-cog'></i></div>
-          <h4 class="title"><a href="">Locally Produced Machineries</a></h4>
+          <h4 class="title">Locally Produced Machineries</h4>
           <p class="description">Machinery is manufactured with local partners and raw materials and easily repaired</p>
         </div>
       </div>
       <div class="col-lg-4 col-md-6" data-aos="zoom-in">
         <div class="box">
           <div class="icon"><i class='bx bx-dollar-circle'></i></div>
-          <h4 class="title"><a href="">Social Enterprise</a></h4>
+          <h4 class="title">Social Enterprise</br></h4>
           <p class="description">Developed a social enterprise business model for sustainability of the project</p>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6" data-aos="zoom-in">
+        <div class="box">
+          <div class="icon"><i class='bx bx-group'></i></div>
+          <h4 class="title">Community</br></h4>
+          <p class="description">They provide support and create awareness among locals for a better livelihood</p>
         </div>
       </div>
     </div>
