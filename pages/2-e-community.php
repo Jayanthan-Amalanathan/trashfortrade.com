adcaf0c8d5589ae0b1eae89fb719b1d3ce2caeac
Abitharani.J <abitharani009@gmail.com>
2020-11-11 22:53:07 +0530

    Add community section images

--- pages/2-e-community.php
+++ pages/2-e-community.php
@@ -11,7 +11,7 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
         <div class="member" data-aos="fade-up" data-aos-delay="100">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-jayanthan.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-kishoth.jpg" class="img-fluid" alt="">
             <div class="social">
               <a href="https://www.linkedin.com/in/jayanthan-amalanathan/"><i class="fa fa-linkedin"></i></a>
             </div>
@@ -26,7 +26,7 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
         <div class="member">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-abi.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-shanjeevan.jpg" class="img-fluid" alt="">
             <div class="social">
             <a href="https://www.linkedin.com/in/abitharani-jeyachandran-8372b6199/"><i class="fa fa-linkedin"></i></a>
             </div>
@@ -41,7 +41,7 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
         <div class="member">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-vicky.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-ahaliya.jpg" class="img-fluid" alt="">
             <div class="social">
             <a href="https://www.linkedin.com/in/vickneshwarasarma-yogeswaraiyer-5b48a81a5/"><i class="fa fa-linkedin"></i></a>
             </div>
@@ -56,7 +56,7 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
         <div class="member">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-mathy.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-anna.jpg" class="img-fluid" alt="">
             <div class="social">
               <a href="https://www.linkedin.com/in/ielamathy-manivannan-80b4901a5/"><i class="fa fa-linkedin"></i></a>
             </div>
@@ -75,7 +75,7 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
         <div class="member" data-aos="fade-up" data-aos-delay="100">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-chandran.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-jathurshan-yokeswaran.jpg" class="img-fluid" alt="">
             <div class="social">
             <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
             </div>
@@ -90,7 +90,7 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
         <div class="member" data-aos="fade-up" data-aos-delay="100">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-chandran.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-jerushan.jpg" class="img-fluid" alt="">
             <div class="social">
             <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
             </div>
@@ -105,7 +105,7 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
         <div class="member" data-aos="fade-up" data-aos-delay="100">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-chandran.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-charuni.jpg" class="img-fluid" alt="">
             <div class="social">
             <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
             </div>
@@ -120,7 +120,7 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
         <div class="member" data-aos="fade-up" data-aos-delay="100">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-chandran.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-grashiyan-dreamspace.jpg" class="img-fluid" alt="">
             <div class="social">
             <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
             </div>
@@ -139,7 +139,7 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
         <div class="member" data-aos="fade-up" data-aos-delay="100">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-chandran.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-kosaaruna-tharmanathan.jpg" class="img-fluid" alt="">
             <div class="social">
             <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
             </div>
@@ -154,7 +154,7 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
         <div class="member" data-aos="fade-up" data-aos-delay="100">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-chandran.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-pavithran.jpg" class="img-fluid" alt="">
             <div class="social">
             <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
             </div>
@@ -169,7 +169,7 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
         <div class="member" data-aos="fade-up" data-aos-delay="100">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-chandran.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-pavithran-thayalanathan.jpg" class="img-fluid" alt="">
             <div class="social">
             <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
             </div>
@@ -184,7 +184,71 @@
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
         <div class="member" data-aos="fade-up" data-aos-delay="100">
           <div class="member-img">
-            <img src="../media\images\team-trash-for-trade\team-trash-for-trade-chandran.jpg" class="img-fluid" alt="">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-prasath.jpg" class="img-fluid" alt="">
+            <div class="social">
+            <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
+            </div>
+          </div>
+          <div class="member-info">
+            <h4>Kirushnachandran Chanthiralingam</h4>
+            <span>Chief Technical Officer</span>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+        <div class="member" data-aos="fade-up" data-aos-delay="100">
+          <div class="member-img">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-shankarie.jpg" class="img-fluid" alt="">
+            <div class="social">
+            <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
+            </div>
+          </div>
+          <div class="member-info">
+            <h4>Kirushnachandran Chanthiralingam</h4>
+            <span>Chief Technical Officer</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+        <div class="member" data-aos="fade-up" data-aos-delay="100">
+          <div class="member-img">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-thanushkar.jpg" class="img-fluid" alt="">
+            <div class="social">
+            <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
+            </div>
+          </div>
+          <div class="member-info">
+            <h4>Kirushnachandran Chanthiralingam</h4>
+            <span>Chief Technical Officer</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+        <div class="member" data-aos="fade-up" data-aos-delay="100">
+          <div class="member-img">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-suthakaran-sivaprakasam.jpg" class="img-fluid" alt="">
+            <div class="social">
+            <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
+            </div>
+          </div>
+          <div class="member-info">
+            <h4>Kirushnachandran Chanthiralingam</h4>
+            <span>Chief Technical Officer</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+        <div class="member" data-aos="fade-up" data-aos-delay="100">
+          <div class="member-img">
+            <img src="../media\images\community-trash-for-trade\community-trash-for-trade-viththiyakaran.jpg" class="img-fluid" alt="">
             <div class="social">
             <a href="https://www.linkedin.com/in/kirushnachandran-chanthiralingam-a016221b4/"><i class="fa fa-linkedin"></i></a>
             </div>
