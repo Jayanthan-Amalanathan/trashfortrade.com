e51ab98c774226b5c946fa220994f277d69f1c52
Abitharani Jeyachandran <abitharani009@gmail.com>
2020-11-21 00:55:05 +0530

    update blog

--- pages/0-2-menu-bar.php
+++ pages/0-2-menu-bar.php
@@ -10,8 +10,8 @@
 
     <nav id="nav-menu-container">
       <ul class="nav-menu">
-        <li class="menu-active"><a href="1-0-index.php">Home</a></li>
-        <li class="menu-has-children"><a href="1-0-index.php#about">About Us</a>
+        <li class="menu-active"><a href="1-0-index.php">Home</a><div class="underline"></div></li>
+        <li class="menu-has-children"><a href="1-0-index.php#about">About Us</a><div class="underline"></div>
           <ul>
             <li><a href="2-0-about.php#team" target="_self">Team</a></li>
             <li><a href="2-0-about.php#supporters">Supporters</a></li>
@@ -20,10 +20,10 @@
             <li><a href="2-0-about.php#community">Community</a></li>
           </ul>
         </li>
-        <li><a href="1-0-index.php#solution">Features</a></li>
-        <li><a href="1-0-index.php#portfolio">Services</a></li>
-        <li><a href="1-0-index.php#blog">Blog</a></li>
-        <li><a href="1-0-index.php#contact">Contact Us</a></li>
+        <li><a href="1-0-index.php#solution">Features</a><div class="underline"></div></li>
+        <li><a href="1-0-index.php#portfolio">Services</a><div class="underline"></div></li>
+        <li><a href="1-0-index.php#blog">Blog</a><div class="underline"></div></li>
+        <li><a href="1-0-index.php#contact">Contact Us</a><div class="underline"></div></li>
       </ul>
     </nav>
     <!-- #nav-menu-container -->
--- pages/1-e-blog.php
+++ pages/1-e-blog.php
@@ -5,9 +5,7 @@
       <h3 class="section-titlex">Blog</h3>
     </div>
     <div class="row">
-      <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1r-IGOnjgFJRYLIdBDaQSP6BMEHuA9YW_ZEIfEb5TR2M&font=Default&lang=en&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen
-        mozallowfullscreen allowfullscreen frameborder='10'></iframe>
-    </div>
+      <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1FJjSftllgmVwglL7EC6min-E6VlWuIpQI7MjxFW-TNw&font=Default&lang=en&start_at_end=true&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe></div>
   </div>
 </div>
 <!--End Blog-->
