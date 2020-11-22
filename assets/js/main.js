bd91e8f1fd40d9e16c1a8228d06b0eaa256afb28
Abitharani Jeyachandran <abitharani009@gmail.com>
2020-11-22 15:19:29 +0530

    change smooth scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,38 +67,33 @@
   } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
     $("#mobile-nav, #mobile-nav-toggle").hide();
   }
+
   // Smooth scroll for the navigation menu and links with .scrollto classes
-var scrolltoOffset = $('#header').outerHeight() - 2;
-$(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    e.preventDefault();
-    var target = $(this.hash);
-    if (target.length) {
+  var scrolltoOffset = $('#header').outerHeight() - 21;
+  $(document).on('click', '.nav-menu a, #mobile-nav a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        e.preventDefault();
 
-      var scrollto = target.offset().top - scrolltoOffset;
+          var scrollto = target.offset().top - scrolltoOffset;
 
-      if ($(this).attr("href") == '#header') {
-        scrollto = 0;
-      }
+          $('html, body').animate({
+            scrollTop: scrollto
+          }, 1500, 'easeInOutExpo');
 
-      $('html, body').animate({
-        scrollTop: scrollto
-      }, 1500, 'easeInOutExpo');
+          if ($(this).parents('.nav-menu').length) {
+            $('.nav-menu .menu-active').removeClass('menu-active');
+            $(this).closest('li').addClass('menu-active');
+          }
 
-      if ($(this).parents('.nav-menu, .mobile-nav').length) {
-        $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-        $(this).closest('li').addClass('active');
-      }
-
-      if ($('body').hasClass('mobile-nav-active')) {
-        $('body').removeClass('mobile-nav-active');
-        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-        $('.mobile-nav-overly').fadeOut();
-      }
-      return false;
-    }
-  }
-});
+          if ($('body').hasClass('mobile-nav-active')) {
+            $('body').removeClass('mobile-nav-active');
+            $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+            $('#mobile-body-overly').fadeOut();
+          }
+          return false;
+            });
 
   // Activate smooth scroll on page load with hash links in the url
   $(document).ready(function() {
