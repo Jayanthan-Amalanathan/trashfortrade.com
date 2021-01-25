d7149c0c6f638fdcbf7ab2f4cd649f22d5c26810
DESKTOP-O6AH0ED\ajaya <ajayanthan97@gmail.com>
2021-01-25 11:21:49 +0530

    Change designation

--- pages/2-a-team.php
+++ pages/2-a-team.php
@@ -7,20 +7,6 @@
     </div>
 
     <div class="row">
-      <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
-        <div class="member" data-aos="fade-up" data-aos-delay="100">
-          <div class="member-img">
-            <img src="../media\images\supporter-trash-for-trade\supporter-trash-for-trade-aravinth.jpg" class="img-fluid" alt="">
-            <div class="social">
-              <a href="https://www.linkedin.com/in/aravinthpanch/"><i class="fa fa-linkedin"></i></a>
-            </div>
-          </div>
-          <div class="member-info">
-            <h4>Aravinth Panch</h4>
-            <span><br>Co-founder<br>Chief Executive Officer</span>
-          </div>
-        </div>
-      </div>
 
       <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
         <div class="member" data-aos="fade-up" data-aos-delay="100">
@@ -33,6 +19,21 @@
           <div class="member-info">
             <h4>Jayanthan Amalanathan</h4>
             <span><br>Co-founder<br>Chief Executive Officer</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+        <div class="member" data-aos="fade-up" data-aos-delay="100">
+          <div class="member-img">
+            <img src="../media\images\supporter-trash-for-trade\supporter-trash-for-trade-aravinth.jpg" class="img-fluid" alt="">
+            <div class="social">
+              <a href="https://www.linkedin.com/in/aravinthpanch/"><i class="fa fa-linkedin"></i></a>
+            </div>
+          </div>
+          <div class="member-info">
+            <h4>Aravinth Panch</h4>
+            <span><br>Co-founder<br>Chief Techical Officer</span>
           </div>
         </div>
       </div>
